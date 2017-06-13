dfa55672bdc57162904dd1d1da18d891c4cadeb8
Luis Arízaga <luis.1142@hotmail.com>
2017-06-12 20:40:35 -0500

    asdsa

---
+++ src/miLista.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "miLista.h"
+
+extern int Lista_Inicializar(ListaEnlazada *lista){
+}
+
+extern int Lista_Vacia(ListaEnlazada *lista){
+}
+
+extern int Lista_Conteo(ListaEnlazada *lista){
+}
+
+extern ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
+}
+
+extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto){
+}
+
+extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){
+}
+
+extern void Lista_Sacar(ListaEnlazada *lista, ElementoLista *elemento){
+}
+
+extern void Lista_SacarTodos(ListaEnlazada *lista){
+}
+
+extern int Lista_InsertarDespues(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
+}
+
+extern int Lista_InsertarAntes(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
+}
+
+extern ElementoLista *Lista_Primero(ListaEnlazada *lista){
+}
+
+extern ElementoLista *Lista_Ultimo(ListaEnlazada *lista){
+}
+
+extern ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento){
+}
+
+extern ElementoLista *Lista_Anterior(ListaEnlazada *lista, ElementoLista *elemento){
+}
+
+
+
+
